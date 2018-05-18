97a421a39febce2f185423fbae02b2596af7bada
ultissasin <31247393+ultissasin@users.noreply.github.com>
2018-05-18 12:29:58 -0500

    jufjuyhgjugjuyfv
    ju j,hfgvjmgfv hgnfvt

--- Hello World/Moto.java
+++ Hello World/Moto.java
@@ -11,10 +11,17 @@ public class Moto
     // instance variables - replace the example below with your own
     public static void main( String[] args )
     {
-        System.out.println( "Hello World" );
-        Motor.B.forward();
-        Motor.C.forward();
+        System.out.println( "Program 1" );
         Button.waitForAnyPress();
+        Motor.A.forward();
+        System.out.println("Forward");
+        Button.waitForAnyPress();
+        Motor.B.backward();
+        Motor.A.backward();
+        System.out.println("Backwards");
+        Button.waitForAnyPress();
+        Motor.A.stop();
+        Motor.B.stop();
         
     }
 }
